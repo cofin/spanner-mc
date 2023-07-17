d6fd7d4132f6aa7d44e342d849cab9a2d64b958b
Cody Fincher <codyfincher@google.com>
2023-07-17 22:07:13 +0000

    fix: otel tweaks

--- spannermc/lib/otel.py
+++ spannermc/lib/otel.py
@@ -4,19 +4,39 @@ from opentelemetry.exporter.cloud_monitoring import (
     CloudMonitoringMetricsExporter,
 )
 from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.instrumentation.grpc import GrpcInstrumentorClient
+from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
 from opentelemetry.propagate import set_global_textmap
-from opentelemetry.propagators.cloud_trace_propagator import CloudTraceFormatPropagator
+from opentelemetry.propagators.cloud_trace_propagator import (
+    CloudTraceFormatPropagator,
+)
+from opentelemetry.resourcedetector.gcp_resource_detector import GoogleCloudResourceDetector
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.resources import Resource, get_aggregated_resources
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.sampling import ParentBasedTraceIdRatio
 
-from . import settings
+from . import db, settings
 
 set_global_textmap(CloudTraceFormatPropagator())
+GrpcInstrumentorClient().instrument()  # type: ignore[no-untyped-call]
+SQLAlchemyInstrumentor().instrument(engine=db.engine)
 staging_labels = {"environment": settings.app.ENVIRONMENT}
+_resources = get_aggregated_resources(
+    [
+        GoogleCloudResourceDetector(),  # type: ignore[no-untyped-call]
+    ],
+    initial_resource=Resource.create(
+        {
+            "service.name": settings.app.NAME,
+            "service.namespace": settings.app.slug,
+            "service.version": settings.app.BUILD_NUMBER,
+        }
+    ),
+    timeout=60,
+)
 meter_provider = MeterProvider(
     metric_readers=[
         PeriodicExportingMetricReader(
@@ -25,20 +45,15 @@ meter_provider = MeterProvider(
             export_timeout_millis=45000,
         )
     ],
-    resource=Resource.create(
-        {
-            "service.name": settings.app.NAME,
-            "service.namespace": settings.app.slug,
-            "service.version": settings.app.BUILD_NUMBER,
-        }
-    ),
+    resource=_resources,
 )
-meter = metrics.get_meter(__name__)
 
+metrics.set_meter_provider(meter_provider)
 # Create and export one trace every 100 requests
-sampler = ParentBasedTraceIdRatio(1 / 500)
-tracer_provider = TracerProvider(sampler=sampler)
-processor = BatchSpanProcessor(CloudTraceSpanExporter())  # type: ignore
+_sampler = ParentBasedTraceIdRatio(1 / 250)
+tracer_provider = TracerProvider(resource=_resources, sampler=_sampler)
 trace.set_tracer_provider(tracer_provider)
-tracer_provider.add_span_processor(processor)
-config = OpenTelemetryConfig(meter=meter, tracer_provider=tracer_provider)
+trace.get_tracer_provider().add_span_processor(  # type: ignore[attr-defined]
+    BatchSpanProcessor(CloudTraceSpanExporter(), max_queue_size=10000)  # type: ignore
+)
+config = OpenTelemetryConfig(tracer_provider=tracer_provider, meter_provider=meter_provider)
