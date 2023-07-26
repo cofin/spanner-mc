624c40e46ee82af00f60747eb7286b3a5b40247f
Cody Fincher <codyfincher@google.com>
2023-07-26 13:44:19 +0000

    fix: test case correction

--- tests/performance/load_tester_kv.py
+++ tests/performance/load_tester_kv.py
@@ -67,15 +67,18 @@ class WebsiteTestUser(FastHttpUser):  # type: ignore
     def kv_write_read_check(self):
         # add event to api
         unique_value = self.unique_value()
+        unique_key = self.unique_key()
         logging.debug("kv_create: event = %s", unique_value)
         with self.client.post(
             self.kv_create_url,
-            json={"message": unique_value},
+            json={"key": unique_key, "value": unique_value},
             headers=self.headers,
             catch_response=True,
         ) as response:
             if response.status_code != 201:
-                error_msg = f"kv_create: response.status_code = {response.status_code}, expected 201, event_message = {unique_value}"
+                error_msg = (
+                    f"kv_create: response.status_code = {response.status_code}, expected 201, kv_key = {unique_value}"
+                )
                 logging.error(error_msg)
                 response.failure(error_msg)
                 raise RescheduleTask()
@@ -83,13 +86,13 @@ class WebsiteTestUser(FastHttpUser):  # type: ignore
             response_dict = response.json()
             if "key" not in response_dict:
                 logger.info(response_dict)
-                error_msg = f"kv_create: key not in response_dict, event_message = {unique_value}"
+                error_msg = f"kv_create: key not in response_dict, kv_key = {unique_value}"
                 logging.error(error_msg)
                 response.failure(error_msg)
                 raise RescheduleTask()
 
             kv_key = response_dict["key"]
-            logging.debug("kv_create: for event_message = %s, kv_key = %s", unique_value, kv_key)
+            logging.debug("kv_create: for kv_key = %s, kv_key = %s", unique_value, kv_key)
 
         # get event from api and check
         with self.client.get(
@@ -99,23 +102,23 @@ class WebsiteTestUser(FastHttpUser):  # type: ignore
             catch_response=True,
         ) as response:
             if response.status_code != 200:
-                error_msg = f"kv_get_by_id: response.status_code = {response.status_code}, expected 200, event_message = {unique_value}"
+                error_msg = f"kv_get_by_id: response.status_code = {response.status_code}, expected 200, kv_key = {unique_value}"
                 logging.error(error_msg)
                 response.failure(error_msg)
                 raise RescheduleTask()
 
-            if "message" not in response_dict:
+            if "value" not in response_dict:
                 logger.info(response_dict)
-                error_msg = f"kv_get_by_id: data not in response_dict, event_message = {unique_value}"
+                error_msg = f"kv_get_by_id: data not in response_dict, kv_key = {unique_value}"
                 logging.error(error_msg)
                 response.failure(error_msg)
                 raise RescheduleTask()
 
-            event_message_returned = response_dict["message"]
-            logging.debug("kv_get_by_id: for kv_key = %s, event_message_returned = %s", kv_key, event_message_returned)
+            kv_value_returned = response_dict["value"]
+            logging.debug("kv_get_by_id: for kv_key = %s, kv_key_returned = %s", kv_key, kv_value_returned)
 
-            if event_message_returned != unique_value:
-                error_msg = f"kv_get_by_id: event_message_returned = {event_message_returned} not equal event_message = {unique_value}"
+            if kv_value_returned != unique_value:
+                error_msg = f"kv_get_by_id: kv_key_returned = {kv_value_returned} not equal kv_key = {unique_value}"
                 logging.error(error_msg)
                 response.failure(error_msg)
                 raise RescheduleTask()
